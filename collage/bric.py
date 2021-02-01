f01a9b8c9456c52b264146a562d4e9bd8ac5b27f
Ștefan Săftescu <stefan@robots.ox.ac.uk>
2021-02-01 02:15:56 +0200

    Add option for computing integer solutions

--- collage/bric.py
+++ collage/bric.py
@@ -3,13 +3,40 @@ import numpy as np
 from collections import namedtuple
 from dataclasses import dataclass
 from numbers import Real
-from typing import List
+from typing import List, Tuple, Union
 
 from .layout import Layout
 
+SizeOrTuple = Tuple[Real, Real]
 
-Size = namedtuple('Size', ['w', 'h'])
-Position = namedtuple('Position', ['x', 'y'])
+
+class Size(namedtuple('Size', ['w', 'h'])):
+    def __add__(self, other: SizeOrTuple):
+        other_w, other_h = other
+        return Size(self.w + other_w, self.h + other_h)
+
+    def __sub__(self, other: SizeOrTuple):
+        other_w, other_h = other
+        return Size(self.w - other_w, self.h - other_h)
+
+    def __round__(self):
+        return Size(*map(round, self))
+
+    def __truediv__(self, divisor: Union[Real, SizeOrTuple]):
+        if isinstance(divisor, Real):
+            return Size(self.w / divisor, self.h / divisor)
+        else:
+            div_w, div_h = divisor
+            return Size(self.w / div_w, self.h / div_h)
+
+
+class Position(namedtuple('Position', ['x', 'y'])):
+    def __add__(self, other: Union[Tuple[Real, Real], Size]):
+        other_w, other_h = other
+        return Position(self.x + other_w, self.y + other_h)
+
+    def __round__(self):
+        return Size(*map(round, self))
 
 
 @dataclass(frozen=True)
@@ -20,41 +47,33 @@ class Constraint:
     result: Real = 0
 
 
-def fit_pictures_balanced(image_sizes, layout='horizontal'):
+def fit_pictures_balanced(image_sizes, layout='horizontal', round_result=False):
     image_sizes = [Size(h, w) for h, w in image_sizes]
     layout = Layout.balanced_layout(len(image_sizes), layout == 'horizontal')
 
-    return fit_pictures(image_sizes, layout)
+    return fit_pictures(image_sizes, layout, round_result)
 
 
-def fit_pictures_grid(image_sizes, *, cols=None, rows=None):
+def fit_pictures_grid(image_sizes, *, cols=None, rows=None, round_result=False):
     image_sizes = [Size(h, w) for h, w in image_sizes]
     layout = Layout.grid_layout(len(image_sizes), cols=cols, rows=rows)
 
-    return fit_pictures(image_sizes, layout)
+    return fit_pictures(image_sizes, layout, round_result)
 
 
-def fit_pictures(image_sizes: List[Size], layout: Layout):
-    new_sizes = _compute_rescaled_image_sizes(image_sizes, layout)
+def fit_pictures(image_sizes: List[Size], layout: Layout, round_result=False):
+    if round_result:
+        new_sizes = _compute_integer_image_sizes(image_sizes, layout)
+    else:
+        new_sizes = _compute_rescaled_image_sizes(image_sizes, layout)
+
     positions, canvas_size = _compute_positions(new_sizes, layout)
 
     return new_sizes, positions, canvas_size
 
 
 def _compute_rescaled_image_sizes(image_sizes: List[Size], layout: Layout):
-    widths, _, constraints = _get_bbox_constraints(
-        layout.root_bounding_box)
-
-    # for N images, the layout will yield N-1 constraints, so we add an extra
-    # one to obtain a unique solution
-    assert layout.n_images - 1 == len(constraints)
-
-    constraints.append(
-        Constraint(is_height=False,
-                   positive_ids=widths,
-                   negative_ids=[],
-                   result=sum([image_sizes[i].w for i in widths]))
-    )
+    constraints = _get_constraints(image_sizes, layout)
 
     # set up and a linear equation system from the constraints, and solve it
     n_images = len(image_sizes)
@@ -80,24 +99,89 @@ def _compute_rescaled_image_sizes(image_sizes: List[Size], layout: Layout):
     return new_sizes
 
 
+def _compute_integer_image_sizes(image_sizes: List[Size], layout: Layout):
+    import mip
+    constraints = _get_constraints(image_sizes, layout)
+    aspect_ratios = [h/w for w, h in image_sizes]
+
+    # set up a mixed-integer program, and solve it
+    n_images = len(image_sizes)
+
+    m = mip.Model()
+    var_widths = [m.add_var(var_type=mip.INTEGER) for _ in range(n_images)]
+    var_heights = [m.add_var(var_type=mip.INTEGER) for _ in range(n_images)]
+
+    for c in constraints:
+        if c.is_height:
+            vars = ([var_heights[i] for i in c.positive_ids] +
+                    [-var_heights[i] for i in c.negative_ids])
+        else:
+            vars = ([var_widths[i] for i in c.positive_ids] +
+                    [-var_widths[i] for i in c.negative_ids])
+
+        m.add_constr(mip.xsum(vars) == c.result)
+
+    # the errors come from a deviation in aspect ratio
+    var_errs = [m.add_var(var_type=mip.CONTINUOUS) for _ in range(n_images)]
+    for err, w, h, ar in zip(var_errs, var_widths, var_heights, aspect_ratios):
+        m.add_constr(err == h - w * ar)
+
+    # To minimise error, we need to create a convex cost function. Common
+    # options are either abs(err) or err ** 2. However, both these functions are
+    # non-linear, so cannot be directly computed in MIP. We can represent abs
+    # exactly with a type-1 SOS, and approximate ** 2 with a type-2 SOS. Here we
+    # use abs.
+
+    var_errs_pos = [m.add_var(var_type=mip.CONTINUOUS) for _ in range(n_images)]
+    var_errs_neg = [m.add_var(var_type=mip.CONTINUOUS) for _ in range(n_images)]
+    var_abs_errs = [m.add_var(var_type=mip.CONTINUOUS) for _ in range(n_images)]
+
+    for abs_err, err, err_pos, err_neg in zip(var_abs_errs,
+                                              var_errs,
+                                              var_errs_pos,
+                                              var_errs_neg):
+        # err_pos and err_neg are both positive representing each side of the
+        # abs function. Only one will be non-zero (SOS Type-1).
+        m.add_constr(err == err_pos - err_neg)
+        m.add_constr(abs_err == err_pos + err_neg)
+        m.add_sos([(err_pos, 1), (err_neg, -1)], sos_type=1)
+
+    m.objective = mip.minimize(mip.xsum(var_abs_errs))
+    m.optimize(max_seconds=30)
+
+    new_sizes = [Size(int(w.x), int(h.x))
+                 for w, h in zip(var_widths, var_heights)]
+    return new_sizes
+
+
+def _get_constraints(image_sizes, layout):
+    constraints = _get_bbox_constraints(layout.root_bounding_box)
+
+    # for N images, the layout will yield N-1 constraints, so we add an extra
+    # one to obtain a unique solution
+    assert layout.n_images - 1 == len(constraints)
+
+    total_width = (layout.width
+                   or sum([image_sizes[i].w
+                           for i in layout.root_bounding_box.width_ids]))
+    constraints.append(
+        Constraint(is_height=False,
+                   positive_ids=layout.root_bounding_box.width_ids,
+                   negative_ids=[],
+                   result=total_width)
+    )
+
+    return constraints
+
+
 def _get_bbox_constraints(bbox):
     if bbox.is_leaf:
-        widths = [bbox.leaf_id]
-        heights = [bbox.leaf_id]
         constraints = []
     else:
-        c_widths, c_heights, c_constraints = zip(*[_get_bbox_constraints(c)
-                                                   for c in bbox.children])
+        constraints = sum([_get_bbox_constraints(c) for c in bbox.children], [])
 
         if bbox.is_horizontal:
-            # widths add up
-            widths = sum(c_widths, [])
-
-            # pick the child with fewest number of height components
-            # to make constraints sparser
-            heights = min(c_heights, key=len)
-
-            constraints = sum(c_constraints, [])
+            c_heights = [c.height_ids for c in bbox.children]
 
             # add a height constraints
             for i in range(len(c_heights) - 1):
@@ -106,14 +190,7 @@ def _get_bbox_constraints(bbox):
                                positive_ids=c_heights[i],
                                negative_ids=c_heights[i + 1]))
         else:
-            # heights add up
-            heights = sum(c_heights, [])
-
-            # pick the child with fewest number of width components
-            # to make constraints sparser
-            widths = min(c_widths, key=len)
-
-            constraints = sum(c_constraints, [])
+            c_widths = [c.width_ids for c in bbox.children]
 
             # add a width constraints
             for i in range(len(c_widths) - 1):
@@ -122,11 +199,11 @@ def _get_bbox_constraints(bbox):
                                positive_ids=c_widths[i],
                                negative_ids=c_widths[i + 1]))
 
-    return widths, heights, constraints
+    return constraints
 
 
 def _compute_positions(image_sizes: List[Size], layout: Layout):
-    def _c(bbox, top_left: Position = Position(0, 0)):
+    def _recurse(bbox, top_left: Position = Position(0, 0)):
         if bbox.is_leaf == 1:
             positions = [(bbox.leaf_id, top_left)]
             size = image_sizes[bbox.leaf_id]
@@ -134,19 +211,19 @@ def _compute_positions(image_sizes: List[Size], layout: Layout):
             positions = []
             size = Size(0, 0)
             for bbox_child in bbox.children:
-                c_positions, c_size = _c(bbox_child, top_left)
+                c_positions, c_size = _recurse(bbox_child, top_left)
                 positions.extend(c_positions)
 
                 if bbox.is_horizontal:
-                    top_left = Position(top_left.x + c_size.w, top_left.y)
-                    size = Size(size.w + c_size.w, c_size.h)
+                    top_left = top_left + (c_size.w, 0)
+                    size = c_size + (size.w, 0)
                 else:
-                    top_left = Position(top_left.x, top_left.y + c_size.h)
-                    size = Size(c_size.w, size.h + c_size.h)
+                    top_left = top_left + (0, c_size.h)
+                    size = c_size + (0, size.h)
 
         return positions, size
 
-    positions, canvas_size = _c(layout.root_bounding_box)
+    positions, canvas_size = _recurse(layout.root_bounding_box)
     assert len(positions) == len(image_sizes)
 
     # sort positions so they match the image size indices
--- collage/layout.py
+++ collage/layout.py
@@ -12,6 +12,8 @@ class BoundingBox:
     is_horizontal: Optional[bool]
     children: _BoundingBoxChildren
     leaf_id: Optional[int]
+    width_ids: List[int]
+    height_ids: List[int]
 
     def __init__(self, is_horizontal: Optional[bool] = None, *,
                  children: _BoundingBoxChildren = None,
@@ -28,6 +30,27 @@ class BoundingBox:
         object.__setattr__(self, 'is_horizontal', is_horizontal)
         object.__setattr__(self, 'children', children)
         object.__setattr__(self, 'leaf_id', leaf_id)
+
+        if self.is_leaf:
+            width_ids = [leaf_id]
+            height_ids = [leaf_id]
+        elif self.is_horizontal:
+            # widths add up
+            width_ids = sum([c.width_ids for c in children], [])
+
+            # pick the child with fewest number of height components
+            # to make constraints sparser
+            height_ids = min([c.height_ids for c in children], key=len)
+        else:
+            # heights add up
+            height_ids = sum([c.height_ids for c in children], [])
+
+            # pick the child with fewest number of height components
+            # to make constraints sparser
+            width_ids = min([c.width_ids for c in children], key=len)
+
+        object.__setattr__(self, 'width_ids', width_ids)
+        object.__setattr__(self, 'height_ids', height_ids)
 
     @property
     def has_children(self):
@@ -47,7 +70,8 @@ class Layout:
     spacing: Optional[Real] = None
 
     @classmethod
-    def balanced_layout(_, n_images, horizontal_root=True) -> 'Layout':
+    def balanced_layout(_, n_images, horizontal_root=True,
+                        width: Optional[Real] = None) -> 'Layout':
         def _build_bbox_tree(start_id, end_id, is_horizontal):
             if end_id - start_id == 1:
                 return BoundingBox(is_horizontal, leaf_id=start_id)
@@ -58,12 +82,13 @@ class Layout:
                 return BoundingBox(is_horizontal, children=[bbox_a, bbox_b])
 
         root_bbox = _build_bbox_tree(0, n_images, horizontal_root)
-        return Layout(n_images, root_bbox)
+        return Layout(n_images, root_bbox, width=width)
 
     @classmethod
     def grid_layout(_, n_images: int, *,
                     cols: Optional[int] = None,
-                    rows: Optional[int] = None) -> 'Layout':
+                    rows: Optional[int] = None,
+                    width: Optional[Real] = None) -> 'Layout':
 
         if cols is None and rows is None:
             raise ValueError("Either cols or rows must be set")
@@ -82,7 +107,7 @@ class Layout:
 
         root_bbox = BoundingBox(root_is_horizontal, children=bboxes)
 
-        return Layout(n_images, root_bbox)
+        return Layout(n_images, root_bbox, width=width)
 
 
 def _group_iter(seq: Iterable, group_size: int):
