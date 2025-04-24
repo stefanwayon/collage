fd5f526a9b585979a344d5db97e24392848d8fd9
Ștefan Săftescu <stefan@saftescu.xyz>
2025-04-25 02:57:40 +0300

    Add more arguments to the CLI demo

--- collage/__main__.py
+++ collage/__main__.py
@@ -2,19 +2,22 @@ from argparse import ArgumentParser
 from math import ceil
 from pathlib import Path
 from PIL import Image, ImageDraw
-from random import randint
 
 from collage.bric import fit_pictures_balanced
 
 
-def make_pil_collage(images,
-                     background_colour=(0, 0, 0, 0),
-                     border_colour=(255, 255, 255, 255),
-                     border=0,
-                     spacing=0):
+def make_pil_collage(
+    images,
+    background_colour=(0, 0, 0, 255),
+    border_colour=(255, 255, 255, 255),
+    width=None,
+    border=0,
+    spacing=0,
+):
     image_sizes = [img.size for img in images]
     new_sizes, positions, canvas_size = fit_pictures_balanced(
-        image_sizes, border=border, spacing=spacing, round_result=True)
+        image_sizes, width=width, border=border, spacing=spacing, round_result=True
+    )
 
     canvas = Image.new('RGBA', (ceil(canvas_size.w), ceil(canvas_size.h)),
                        background_colour)
@@ -35,14 +38,37 @@ def make_pil_collage(images,
 
 
 parser = ArgumentParser('collage')
-parser.add_argument('--border', default=0, type=int)
-parser.add_argument('--spacing', default=0, type=int)
-parser.add_argument('image_file', nargs='+', type=Path)
+parser.add_argument(
+    "--border",
+    default=0,
+    type=int,
+    help="Width of the border around each image (in pixels). "
+    "The border is drawn around each image individually.",
+)
+parser.add_argument(
+    "--spacing",
+    default=0,
+    type=int,
+    help="Spacing between images (in pixels). "
+    "The spacing is the gap between adjacent images.",
+)
+parser.add_argument(
+    "--output_width",
+    default=None,
+    type=int,
+    help="Width of the output image (in pixels). "
+    "If not specified, the output width is determined automatically.",
+)
+parser.add_argument(
+    "--output", default="output.png", type=str, help="Output image file name."
+)
+parser.add_argument(
+    "image_file", nargs="+", type=Path, help="Paths to the input image files."
+)
 args = parser.parse_args()
 
 images = [Image.open(f) for f in args.image_file]
 
-
-# images = [Image.new('RGB', (320, 220), tuple([randint(0, 150) for _ in range(3)]))
-#           for _ in range(3)]
-make_pil_collage(images, border=args.border, spacing=args.spacing).save('asdf.png')
+make_pil_collage(
+    images, width=args.output_width, border=args.border, spacing=args.spacing
+).save(args.output)
